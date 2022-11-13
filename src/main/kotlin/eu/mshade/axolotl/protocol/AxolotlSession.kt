8493c8356dfdce16634b88eef17193dd651a75b8
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-13 17:06:52 +0100

    add AxolotlSession 🧤

---
+++ src/main/kotlin/eu/mshade/axolotl/protocol/AxolotlSession.kt
@@ -0,0 +1,31 @@
+package eu.mshade.axolotl.protocol
+
+import eu.mshade.enderframe.entity.Entity
+import eu.mshade.enderframe.entity.Player
+import eu.mshade.enderframe.world.World
+import eu.mshade.enderframe.world.chunk.Chunk
+import io.netty.channel.Channel
+
+abstract class AxolotlSession(val channel: Channel) {
+
+    fun sendPacket(axolotlPacketOut: AxolotlPacketOut) {
+        channel.writeAndFlush(axolotlPacketOut)
+    }
+
+    abstract fun sendInitializePlayer(player: Player)
+
+    abstract fun sendPlayerJoin(player: Player)
+
+    abstract fun sendPlayerLeave(player: Player)
+
+    abstract fun sendEntityLocation(entity: Entity)
+
+    abstract fun sendChunk(chunk: Chunk)
+
+    abstract fun sendChunkUnload(chunk: Chunk)
+
+    abstract fun sendInitializeWorld(world: World)
+
+
+
+}

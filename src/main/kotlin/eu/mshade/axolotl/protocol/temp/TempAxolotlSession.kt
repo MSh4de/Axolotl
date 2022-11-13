e6bb8b0710f76a7c53ec0f2a9e0fc9ca90286b5b
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-13 17:06:18 +0100

    add TempAxolotlSession 🎨

---
+++ src/main/kotlin/eu/mshade/axolotl/protocol/temp/TempAxolotlSession.kt
@@ -0,0 +1,41 @@
+package eu.mshade.axolotl.protocol.temp
+
+import eu.mshade.axolotl.protocol.AxolotlSession
+import eu.mshade.enderframe.entity.Entity
+import eu.mshade.enderframe.entity.Player
+import eu.mshade.enderframe.world.World
+import eu.mshade.enderframe.world.chunk.Chunk
+import io.netty.channel.Channel
+
+class TempAxolotlSession(channel: Channel) : AxolotlSession(channel) {
+
+    override fun sendInitializePlayer(player: Player) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendPlayerJoin(player: Player) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendPlayerLeave(player: Player) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendEntityLocation(entity: Entity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendChunk(chunk: Chunk) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendChunkUnload(chunk: Chunk) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendInitializeWorld(world: World) {
+        TODO("Not yet implemented")
+    }
+
+
+}

d6ee291a7f20d751526fb3a54b3be20f2537f97e
RealAlphaUA <realalpha.ua@gmail.com>
2022-12-06 00:03:47 +0100

    add toString 🎨

--- src/main/kotlin/eu/mshade/axolotl/event/HandshakeAxolotlEvent.kt
+++ src/main/kotlin/eu/mshade/axolotl/event/HandshakeAxolotlEvent.kt
@@ -3,4 +3,10 @@ package eu.mshade.axolotl.event
 import eu.mshade.axolotl.protocol.AxolotlProtocolVersion
 import eu.mshade.axolotl.protocol.AxolotlSession
 
-class HandshakeAxolotlEvent(val axolotlSession: AxolotlSession, val axolotlProtocolVersion: AxolotlProtocolVersion): AxolotlEvent
+class HandshakeAxolotlEvent(val axolotlSession: AxolotlSession, val axolotlProtocolVersion: AxolotlProtocolVersion) : AxolotlEvent {
+
+    override fun toString(): String {
+        return "HandshakeAxolotlEvent(axolotlSession=$axolotlSession, axolotlProtocolVersion=$axolotlProtocolVersion)"
+    }
+
+}
